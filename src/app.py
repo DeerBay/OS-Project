31b65fb6d161fde33e68e0e29ecf04163e365378
DeerBay <juliamariasjoholm@gmail.com>
2023-11-20 22:21:44 +0100

    DashTools Automatic Push - 11/20/2023 @ 22:21:44

--- src/app.py
+++ src/app.py
@@ -20,6 +20,8 @@ app = Dash(__name__,
                meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}]
 )
 
+server = app.server
+
 app.layout = html.Div([
     dbc.Row(
         [
