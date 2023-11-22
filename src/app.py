36f7946cf429928eaaea067bbe97f3d73ebba4b7
DeerBay <juliamariasjoholm@gmail.com>
2023-11-22 10:22:16 +0100

    Update with Sweden Graphs

--- src/app.py
+++ src/app.py
@@ -74,7 +74,7 @@ app.layout = html.Div([
         dbc.Col(  
             dbc.Card(
                 [
-                    dbc.CardHeader(html.H3("Workning Title Graph One Right", className="text-body-tertiary", id="header_graph_2_right")),
+                    dbc.CardHeader(html.H3("Workning Title Graph One Right", className="text-body-tertiary", id="header_graph_1_right")),
                     dbc.CardBody([
                         dcc.Dropdown(id='country_dropdown_right', 
                                  className='ml-3 mr-3 mb-1 text-info', 
@@ -87,24 +87,104 @@ app.layout = html.Div([
             ), xs=12, sm=11, md=10, lg=5
         ),
     ], justify='evenly'),
+
     dbc.Row([
-        dbc.Button("Reset", id='reset-button', className='float-left', n_clicks=0), # Callback to reset page
-        dcc.Link("Contributors", href="https://github.com/DeerBay/OS-Project/graphs/contributors", target="_blank"), # "_blank": Opens the linked document in a new tab or window.
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardHeader(html.H3("Sweden top 10", className="text-body-tertiary", id="header_graph_2_left")),
+                    dbc.CardBody([
+                                 dcc.Graph(id="graph_2_left", figure={}),
+                                 dcc.Graph(id="graph_2_right", figure={}),
+                    ]),
+                ],
+                className="mb-3",
+            ), xs=12, sm=11, md=10, lg=5
+        ),
+    ], justify='evenly'),
+
+    dbc.Row([
+        dbc.Button("Reset", id='reset-button', href="https://iths-olympics.onrender.com",  className='float-left', title='Resets all graphs'), # reloads the page in same window
+        dcc.Link("Contributors", href="https://github.com/DeerBay/OS-Project/graphs/contributors", target="_blank", title='Link to repository on GitHub'), # "_blank": Opens the linked document in a new tab or window.
     ],style={"margin-top": "20px", "text-align": "center"})
 ])
 
+# Callback to update the graph based on dropdown selections
 @callback(
-    Output("graph_1_left", "figure"),
-    )
+    Output("graph_2_left", "figure"),
+    [
+        Input('year_dropdown', 'value'),
+        Input('sport_dropdown', 'value'),
+        Input('season_dropdown', 'value'),
+    ]
+)
+def update__top10_graph(year, sport, season):
+    # Filter the data based on the selected values
+    filtered_data = sweden_athletes
+    if year:
+        filtered_data = filtered_data[filtered_data['Year'].isin(year)]
+    if sport:
+        filtered_data = filtered_data[filtered_data['Sport'].isin(sport)]
+    if season:
+        filtered_data = filtered_data[filtered_data['Season'].isin(season)]
 
-def figure_one(year, country):
-    df = athlete_events.query("Year == @year & Country == @country")
-    # Dataframe for participants and medals per year
-    participants_medals= df.groupby(['Year', 'Season', 'Country', 'Continent','Country_latitude', 'Country_longitude','Continent_latitude', 'Continent_longitude', 'Sport'], as_index=False)[['Name', 'Medal']].agg(
-    {'Name': 'nunique', 'Medal': 'count'})
-    return ...
-    ...
+    # Grouping by sport and counting medals
+    medals_per_sport = filtered_data.groupby("Sport", as_index=False)["Medal"].count()
+
+    # Sort values on number of medals in descending order, resetting index and displaying the result in a plot.
+    fig = px.bar(
+        data_frame=medals_per_sport.sort_values(by='Medal', ascending=False, ignore_index=True).head(10),
+        x='Sport',
+        y='Medal',
+        labels={'Medal': 'Number of Medals'},
+        template='plotly_white',
+        title="Top 10 Sports with the Most Medals",
+        barmode='overlay',
+    )
+    fig.update_xaxes(tickangle=45)
+
+    return fig
+
+# Callback to update the graph based on dropdown selections
+@callback(
+    Output("graph_2_right", "figure"),
+    [
+        Input('year_dropdown', 'value'),
+        Input('sport_dropdown', 'value'),
+        Input('season_dropdown', 'value'),
+
+    ]
+)
+def update_top10_gold_graph(year, sport, season):
+    # Filter the data based on the selected values
+    filtered_data = sweden_athletes
+    if year:
+        filtered_data = filtered_data[filtered_data['Year'].isin(year)]
+    if sport:
+        filtered_data = filtered_data[filtered_data['Sport'].isin(sport)]
+    if season:
+        filtered_data = filtered_data[filtered_data['Season'].isin(season)]
+
+    # Grouping by sport and counting medals
+    medals_per_sport = filtered_data.groupby(["Sport", "Medal"], as_index=False).size()
+
+    # Filter to include only Gold medals
+    medals_per_sport = medals_per_sport[medals_per_sport['Medal'].isin(['Gold'])]
+
+    # Sort values on the number of medals in descending order, resetting index and displaying the result in a plot.
+    fig = px.bar(
+        data_frame=medals_per_sport.sort_values(by='size', ascending=False, ignore_index=True).head(10),
+        x='Sport',
+        y='size',
+        labels={'size': 'Number of Medals'},
+        title="Top 10 Sports Gold Medals",
+        barmode='group',
+    )
+    fig.update_xaxes(tickangle=45)
+
+    return fig
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port='3232')
     
